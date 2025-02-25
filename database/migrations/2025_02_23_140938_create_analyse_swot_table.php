1941ee611afc682e1e9d0de61343ac5c48fcff46
root <daniel.ntaye@sahanalytics.com>
2025-02-25 09:23:09 +0000

    create analyisi pages

--- database/migrations/2025_02_23_140938_create_analyse_swot_table.php
+++ database/migrations/2025_02_23_140938_create_analyse_swot_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('analyse_swot', function (Blueprint $table) {
+        Schema::create('analyse_swots', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('categorie');
